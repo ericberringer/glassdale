01a9a88a20f63a18672814ce906e3ded45d5fd43
ericberringer <eric.berringer@gmail.com>
2021-01-26 10:36:49 -0600

    Brought in the ConvictionSelect code from git hub, imported get and useConviction functions, const contentTarget to select which container to use on the DOM, export ConvictionSelect function, invoke getConvictions which fetches the data from the api, add a .then and assign useConvictions() to a new variable of convictions render(convictions) close function. New variable const render = convictionsCollection function, this function sends innerHTML to our contentTarget on the DOM, uses the select tag to create a dropdown menu, an option tag to hold a please select crime placeholder option, and we use convictionsCollection.map() whose parameter will be a callback function called conviction which includes a string with an option html tag, string interpolation to include conviction.id assigned to value, and conviction.name in the body of the option tag so that will show up in the menu.

--- scripts/convictions/ConvictionProvider.js
+++ scripts/convictions/ConvictionProvider.js
@@ -1,4 +1,10 @@
-export const useConvictions = () => {
+let convictions = []
+
+export const useConvictions = () => convictions.slice()
+
+export const getConvictions = () => {
     return fetch("https://criminals.glassdale.us/crimes")
-    
-}
+            .then(response => response.json())
+            .then(convictionsArray => convictions = convictionsArray)
+            }
+                
--- scripts/criminals/CriminalDataProvider.js
+++ scripts/criminals/CriminalDataProvider.js
@@ -7,13 +7,11 @@ export const useCriminals = () => {
 export const getCriminals = () => {
     return fetch("https://criminals.glassdale.us/criminals")
         .then(response => response.json())
-        .then(parsedCriminals => {
-            console.table(parsedCriminals)
-            criminals = parsedCriminals
+        .then(parsedCriminals => criminals = parsedCriminals)
         /*
             Load database state into application state with a fetch().
             Make sure the last then() updates the criminals array
         */
-            }
-        )
+            
+    
 }
