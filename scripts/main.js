f768fb28e11aaaa02731790e8df46bc95bfa5b5b
ericberringer <eric.berringer@gmail.com>
2021-01-29 11:15:08 -0600

    Finished dropdown menu and text fields for notes, not functional yet, populated Note.js and NoteList.js with basic code from the chapter, but have not touched it yet.

--- scripts/main.js
+++ scripts/main.js
@@ -5,10 +5,12 @@ import { ConvictionSelect } from './convictions/ConvictionSelect.js'
 import { OfficerList } from './officers/OfficerList.js'
 import { OfficerSelect } from './officers/OfficerSelect.js'
 import { NoteForm } from './notes/NoteForm.js'
+import { ShowNoteButton } from './notes/ShowNotesButton.js'
 
 
 // getConvictions()
 // .then(() => console.log(useConvictions()))
+ShowNoteButton()
 NoteForm()
 CriminalList()
 ConvictionSelect()
---
+++ scripts/notes/ShowNotesButton.js
@@ -0,0 +1,16 @@
+const contentTarget = document.querySelector(".noteListButton")
+const eventHub = document.querySelector(".container")
+
+// Here we are listening for a click event on the show notes button, if the showNotes button is clicked dispatch
+// the custom event which sends a flare up that the button was clicked.
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "showNotes") {
+        const customEvent = new CustomEvent("showNotesClicked")
+        eventHub.dispatchEvent(customEvent)
+    }
+})
+// The show note button is rendered to the DOM on page load by exporting this function. ShowNoteButton targets our button container
+// and sends the html representation of the button there.
+export const ShowNoteButton = () => {
+    contentTarget.innerHTML = "<button id='showNotes'>Show Notes</button>"
+}
