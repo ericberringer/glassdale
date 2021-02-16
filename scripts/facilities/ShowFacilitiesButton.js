5bad576f3e0fb316ef32341c0d41c5cdcf7a1027
ericberringer <eric.berringer@gmail.com>
2021-02-16 16:03:09 -0600

    started on my show facilities button.

--- scripts/facilities/ShowFacilitiesButton.js
+++ scripts/facilities/ShowFacilitiesButton.js
@@ -1,7 +1,15 @@
 
+// need contentTarget, event Hub. Need to get the criminals
+// and facilities. 
+// filter the facilities by their criminals.
+// need custom event on button click that dispatches a custom event called facilitiesButtonClicked.
+
+const contentTarget = document.querySelector(".facilitiesButton")
+
 
 export const ShowFacilitiesButton = (facility) => {
     return `
-    <button id="associates--${facility.id}">List Facilities</button>
+    <button class="facilitiesButton" id="associates--${facility.id}">List Facilities</button>
     `
 }
+
